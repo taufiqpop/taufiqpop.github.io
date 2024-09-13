622b880e7350e79ad6da1a104dfa3b196713f529
taufiqpop <taufiqpop999@gmail.com>
2024-09-14 00:32:43 +0700

    update link portfolio

--- js/script.js
+++ js/script.js
@@ -304,7 +304,7 @@
 
 // Live Chat
 window.$crisp = [];
-window.CRISP_WEBSITE_ID = "d1a3310d-2d9a-484d-9e88-239aa90cfd99";
+window.CRISP_WEBSITE_ID ={{ "d1a3310d-2d9a-484d-9e88-239aa90cfd99"}};
 (function () {
   d = document;
   s = d.createElement("script");
